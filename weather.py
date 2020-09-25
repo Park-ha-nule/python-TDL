48f8b460b5f64ea92b911be21e6a3f27d3261a90
Park-ha-nule <sodlfma4869@naver.com>
2020-09-25 14:11:57 +0900

    if

--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(html.text, 'html.parser')
 
 # 주간예보를 먼저 불러옴 (중복되는 클래스가 있어서 전체로 안하고 주간예보로 먼저 짜르고 시작함)
 data0 = soup.find('div', {'id' : 'weekly'})
-# print('주간 예보 : ')
+print('주간 예보 : ')
 # print(data0)
 
 # 주간 예보 안에 내가 필요한 일주일 날씨 박스만 불러옴
@@ -22,9 +22,30 @@ data1 = data0.find('div', {'class':'scroll_area'})
 # 박스안에 날씨 부분 (맑음)이 속한 태그 불러오기
 find_weather = data1.find_all('i', {'class' : 'ico _cnLazy'})
 for item in find_weather:
-    print(item.find('span').text)
+    weather = item.find('span').text
+    sunny = "맑음"
+    cloud = "구름"
+    cloudy = "흐림"
+    rain = "비"
+    fog = "안개"
+    if sunny in weather:
+        print("맑음")
+    elif cloud:
+        print("구름 많음")
+    elif cloudy:
+        print("흐림")
+    elif rain:
+        print("비")
+    elif fog:
+        print("안개")
+    else:
+        print("None")
 
-# i 태그 안에 real weather 불러오기
+
+# for item in find_weather:
+#     print(item.find('span').text)
+
+
 
 
 # # 이후에 온도정보도 추출해보자
