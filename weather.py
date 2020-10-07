4386be888c440dbd338060c6a328dffc58a355d2
Park-ha-nule <sodlfma4869@naver.com>
2020-10-07 12:02:40 +0900

    map

--- weather.py
+++ weather.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 # pprint 는 딕셔너리의 데이터가 긴 경우에 좀 더 보기 편하게 도와준다.
 from pprint import pprint
 import requests
+import numpy as np
+
+
+def getIconNumber(weather):
+    return weather['data-ico'][-1]
+
 
 # 웹페이지 요청을 하는 코드. 특정 url을 적으면 웹페이지에 대한 소스코드를 볼 수 있음
 html = requests.get('https://n.weather.naver.com/today/09140104')
@@ -13,33 +19,36 @@ soup = BeautifulSoup(html.text, 'html.parser')
 # 주간예보를 먼저 불러옴 (중복되는 클래스가 있어서 전체로 안하고 주간예보로 먼저 짜르고 시작함)
 data0 = soup.find('div', {'id' : 'weekly'})
 print('주간 예보 : ')
-# print(data0)
 
 # 주간 예보 안에 내가 필요한 일주일 날씨 박스만 불러옴
 data1 = data0.find('div', {'class':'scroll_area'})
-# print(data1)
 
 # 박스안에 날씨 부분 (맑음)이 속한 태그 불러오기
 find_weather = data1.find_all('i', {'class' : 'ico _cnLazy'})
-for item in find_weather:
-    weather = item.find('span').text
-    sunny = "맑음"
-    cloud = "구름"
-    cloudy = "흐림"
-    rain = "비"
-    fog = "안개"
-    if sunny in weather:
-        print("맑음")
-    elif cloud:
-        print("구름 많음")
-    elif cloudy:
-        print("흐림")
-    elif rain:
-        print("비")
-    elif fog:
-        print("안개")
-    else:
-        print("None")
+
+find_weather_icon = list(map(getIconNumber, find_weather))
+print(find_weather_icon)
+
+
+# for item in find_weather:
+#     weather = item.find('span').text
+#     sunny = "맑음"
+#     cloud = "구름"
+#     cloudy = "흐림"
+#     rain = "비"
+#     fog = "안개"
+#     if sunny in weather:
+#         print("맑음")
+#     elif cloud:
+#         print("구름 많음")
+#     elif cloudy:
+#         print("흐림")
+#     elif rain:
+#         print("비")
+#     elif fog:
+#         print("안개")
+#     else:
+#         print("None")
 
 
 # for item in find_weather:
